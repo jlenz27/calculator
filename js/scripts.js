0e3d72bf4ab21b12f5fcf9a6b45219e19b55a487
John Lenz <john.lenz27@gmail.com>
2023-01-09 14:13:03 -0800

    Added all the results in one line

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 // business logic
 function add(number1, number2) {
   const result = number1 + number2;
-  const newResult = "The addition of your numbers = " + result;
+  const newResult = number1 + "+" + number2 +"=" + result;
   return newResult;
 }
 
 function subtract(number1, number2) {
   const result = number1 - number2;
-  const newResult = "The subtraction of your numbers = " + result;
+  const newResult = number1 + "-" + number2 +"=" + result;
   return newResult;
 }
 
 function divide(number1, number2) {
   const result = number1 / number2;
-  const newResult = "The divide of your numbers = " + result;
+  const newResult = number1 + "/" + number2 +"=" + result;
   return newResult;
 
 }
 
 function multiply(number1, number2) {
   const result = number1 * number2;
-  const newResult = "The multiply of your numbers = " + result;
+  const newResult = number1 + "*" + number2 +"=" + result;
   return newResult;
 }
 
@@ -33,4 +33,6 @@ const number2 = parseInt(prompt("Enter another number:"));
 // window.alert(divide(number1, number2));
 // window.alert(multiply(number1, number2));
 
-window.alert(add(number1, number2)+ "\n" + subtract (number1, number2) + "\n" + divide (number1, number2) + "\n" + multiply (number1, number2));
+// window.alert(add(number1, number2)+ "\n" + subtract (number1, number2) + "\n" + divide (number1, number2) + "\n" + multiply (number1, number2));
+
+window.alert(add(number1, number2)+ ", " + subtract (number1, number2) + ", " + divide (number1, number2) + ", " + multiply (number1, number2));
