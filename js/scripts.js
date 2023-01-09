105ce61c2007db475fe994533a2a9d90f60dc021
John Lenz <john.lenz27@gmail.com>
2023-01-09 14:01:25 -0800

    it will add all the calculations on one alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 // business logic
 function add(number1, number2) {
-  return number1 + number2;
+  const result = number1 + number2;
+  const newResult = "The addition of your numbers = " + result;
+  return newResult;
 }
 
 function subtract(number1, number2) {
-  return number1 - number2;
+  const result = number1 - number2;
+  const newResult = "The subtraction of your numbers = " + result;
+  return newResult;
 }
 
 function divide(number1, number2) {
-  return number1 / number2;
+  const result = number1 / number2;
+  const newResult = "The divide of your numbers = " + result;
+  return newResult;
+
 }
 
 function multiply(number1, number2) {
-  return number1 * number2;
+  const result = number1 * number2;
+  const newResult = "The multiply of your numbers = " + result;
+  return newResult;
 }
 
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-window.alert(subtract(number1, number2));
-window.alert(divide(number1, number2));
-window.alert(multiply(number1, number2));
+// window.alert(add(number1, number2));
+// window.alert(subtract(number1, number2));
+// window.alert(divide(number1, number2));
+// window.alert(multiply(number1, number2));
+
+window.alert(add(number1, number2)+ "\n" + subtract (number1, number2) + "\n" + divide (number1, number2) + "\n" + multiply (number1, number2));
